9fd0c355d74a9e08fa86e93e22c3cc9a85613102
shreemarella <mvsrikanth96@gmail.com>
2020-04-08 07:59:29 -0400

    [SM] created splash

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_midterm/ui/SplashScreenActivity.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_midterm/ui/SplashScreenActivity.java
@@ -1,4 +1,21 @@
 package com.shreemarella.c0771612_w2020_mad3125_midterm.ui;
 
-public class SplashScreenActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.shreemarella.c0771612_w2020_mad3125_midterm.R;
+
+public class SplashScreenActivity extends AppCompatActivity
+{
+    private int SPLASH_TIMING = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac);
+
+        new
+    }
 }
