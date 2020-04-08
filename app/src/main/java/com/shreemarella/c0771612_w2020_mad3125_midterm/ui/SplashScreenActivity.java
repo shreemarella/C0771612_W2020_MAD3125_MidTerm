41734ffeb536744d7c9f9233a49d254d0992caa0
shreemarella <mvsrikanth96@gmail.com>
2020-04-08 08:24:27 -0400

    [[SM] designed splash screen for edit text of name

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_midterm/ui/SplashScreenActivity.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_midterm/ui/SplashScreenActivity.java
@@ -14,8 +14,9 @@ public class SplashScreenActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ac);
+        setContentView(R.layout.activity_splash);
 
-        new
+
+
     }
 }
