b8d5da465bf19b61d3ed08c65239c660bd193288
shreemarella <mvsrikanth96@gmail.com>
2020-04-09 13:59:34 -0400

    [SM] DIsplaying Screen designing

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_midterm/DisplayScreen.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_midterm/DisplayScreen.java
@@ -3,8 +3,28 @@ package com.shreemarella.c0771612_w2020_mad3125_midterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class DisplayScreen extends AppCompatActivity {
+public class DisplayScreen extends AppCompatActivity
+{
+
+    private TextView TotalTaxPaid;
+    private TextView fullname;
+    private TextView BirthDate;
+    private TextView Age;
+    private TextView Date;
+    private TextView Gender;
+    private TextView sinnumber;
+    private TextView GrossIncome;
+    private TextView CPP;
+    private TextView EI;
+    private TextView CFRSSP;
+    private TextView RRSP;
+    private TextView TotalTaxableIncome;
+    private TextView ProvincialTax;
+    private TextView FederalTax;
+    
+    public static final String CRA_Customer = "CRA Customer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
