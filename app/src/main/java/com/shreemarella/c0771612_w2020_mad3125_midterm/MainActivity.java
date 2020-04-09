7de9f84dfa264b2300f8e0d56ccb138a12f709b8
shreemarella <mvsrikanth96@gmail.com>
2020-04-09 16:57:21 -0400

    [SM] Tax filing screen modifying

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_midterm/MainActivity.java
@@ -2,13 +2,29 @@ package com.shreemarella.c0771612_w2020_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+import com.shreemarella.c0771612_w2020_mad3125_midterm.MainActivity;
+
+public class MainActivity extends AppCompatActivity
+{
+    private int SPLASH_TIMING = 5000;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent hint = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(hint);
+                finish();
+            }
+        },SPLASH_TIMING);
     }
 }
