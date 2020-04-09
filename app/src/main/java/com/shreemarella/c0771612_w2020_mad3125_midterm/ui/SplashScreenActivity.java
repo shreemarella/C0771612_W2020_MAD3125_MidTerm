a1b57d7c22b8122358f4e85e256e28f42a0d186c
shreemarella <mvsrikanth96@gmail.com>
2020-04-08 21:09:30 -0400

    [[SM] designed splash screen total with image view and text ids

--- app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_midterm/ui/SplashScreenActivity.java
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_midterm/ui/SplashScreenActivity.java
@@ -1,9 +1,12 @@
 package com.shreemarella.c0771612_w2020_mad3125_midterm.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.shreemarella.c0771612_w2020_mad3125_midterm.MainActivity;
 import com.shreemarella.c0771612_w2020_mad3125_midterm.R;
 
 public class SplashScreenActivity extends AppCompatActivity
@@ -15,6 +18,15 @@ public class SplashScreenActivity extends AppCompatActivity
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent hint = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(hint);
+                finish();
+            }
+        },SPLASH_TIMING);
 
 
 
