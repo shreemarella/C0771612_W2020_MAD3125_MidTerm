f018e7bab4966eff1541841c291246775f46f5a2
shreemarella <mvsrikanth96@gmail.com>
2020-04-09 09:31:36 -0400

    .

---
+++ app/src/main/java/com/shreemarella/c0771612_w2020_mad3125_midterm/DataEntry.java
@@ -0,0 +1,14 @@
+package com.shreemarella.c0771612_w2020_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_entry);
+    }
+}
